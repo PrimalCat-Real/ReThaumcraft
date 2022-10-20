2c3cdf18f1625de4f50348c807907bb67cb16739
PrimalCat-Real <romanenko.denis2003@gmail.com>
2022-10-20 20:33:33 +0300

    clean draw text

--- src/main/java/thaumcraft/thaumcraft/client/event/RegisterHud.java
+++ src/main/java/thaumcraft/thaumcraft/client/event/RegisterHud.java
@@ -23,65 +23,122 @@ import thaumcraft.thaumcraft.common.init.ItemInit;
 @Mod.EventBusSubscriber(modid = Thaumcraft.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 
 public class RegisterHud {
-
-    // TODO use for node render
     @SubscribeEvent
     public static void renderSelectedBlocks(final RenderLevelStageEvent event) {
         if(event.getStage() != RenderLevelStageEvent.Stage.AFTER_PARTICLES)
             return;
-        if(Minecraft.getInstance().player != null && Minecraft.getInstance().player.getMainHandItem().getItem() == ItemInit.THAUMOMETER.get()) {
-            int combinedLightIn = Minecraft.getInstance().getEntityRenderDispatcher().getPackedLightCoords(Minecraft.getInstance().player, event.getPartialTick());
+        if(Minecraft.getInstance().player != null) {
+            Minecraft mc = Minecraft.getInstance();
             PoseStack matrix = event.getPoseStack();
-            MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
-            VertexConsumer builder = buffer.getBuffer(RenderTypes.THICK_LINES);
             Vec3 playerPos = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
-            BlockPos blocks = new BlockPos(0,-61, 0);
-            AABB box = new AABB(blocks);
-            Matrix4f matrix4f = matrix.last().pose();
+            MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
             matrix.pushPose();
-            matrix.translate(-playerPos.x(), -playerPos.y(), -playerPos.z());
-            matrix.scale(1.1F,1F,1F);
-            LevelRenderer.renderLineBox(matrix, builder, box, 1.0F, 0.0F, 0.0F, 1.0F);
-//            new ItemRenderer().;
-
-//            IClientItemExtensions.FontContext fontrenderer;
-            float width = (float)(-Minecraft.getInstance().font.width("test") / 2);
-            TranslatableContents text = new TranslatableContents("Test");
-//            Minecraft.getInstance().font.drawInBatch("test", width, 1F,0,false, matrix4f, buffer, true, 0, combinedLightIn);
-//            FontRenderer fontrenderer = Minecraft.getInstance().font;
-//            fontrenderer.drawInBatch(text, width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
-//            LevelRenderer.renderLineBox(matrix,builder, 1D,0D,1D,1D,0D,0D, 1.0F, 0.0F, 0.0F, 1.0F);
-            renderNameTag(matrix, buffer, event.getPartialTick(), 0.0F);
-            matrix.popPose();
-            buffer.endBatch(RenderTypes.THICK_LINES);
+            matrix.translate(-playerPos.x, -playerPos.y, -playerPos.z);
+            renderNameTag(mc,matrix,buffer, event.getPartialTick());
         }
     }
 
-    private static void renderNameTag(PoseStack ms, MultiBufferSource.BufferSource buffer, float partialTicks, float yOffset) {
-        Minecraft mc = Minecraft.getInstance();
-        var playerLookX = mc.player.getLookAngle().x;
-        var playerLookY = mc.player.getLookAngle().y;
-        var playerLookZ = mc.player.getLookAngle().z;
-        Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
+    private static void renderNameTag(Minecraft mc, PoseStack matrix, MultiBufferSource.BufferSource buffer, float partialTicks) {
         int combinedLightIn = mc.getEntityRenderDispatcher().getPackedLightCoords(mc.player, partialTicks);
-        ms.pushPose();
- /*       System.out.println(mc.player.getX());*/
-        ms.translate(mc.player.getX(), mc.player.getY(), mc.player.getZ());
+        matrix.translate(0, -60, 0);
         Quaternion q = mc.getEntityRenderDispatcher().cameraOrientation();
-        ms.mulPose(q);
-        ms.scale(-0.025F, -0.025F, 0.025F);
-        Matrix4f matrix4f = ms.last().pose();
-
-        float opacity = Minecraft.getInstance().options.getBackgroundOpacity(0.5F);
-        int alpha = (int)(opacity * 255.0F) << 24;
-        Font fontrenderer = Minecraft.getInstance().font;
-        float width = (float)(-fontrenderer.width("test") / 2);
-        //fontrenderer.drawInBatch(text, f2, i, 553648127, false, matrix4f, buffer, false, j, p_225629_5_);
-
-        //fontrenderer.drawInBatch(text, width, 0f, 553648127, false, matrix4f, buffer, false, alpha, combinedLightIn);
-        fontrenderer.drawInBatch("test", width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
-        ms.popPose();
+        matrix.mulPose(q);
+        matrix.scale(-0.025F, -0.025F, 0.025F);
+        Matrix4f matrix4f = matrix.last().pose();
+        Font fontRenderer = Minecraft.getInstance().font;
+        float width = (float)(-fontRenderer.width("test") / 2);
+        fontRenderer.drawInBatch("test", width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
+        matrix.popPose();
     }
+//    @SubscribeEvent
+//    public static void renderSelectedBlocks(final RenderLevelStageEvent event) {
+//        // prevent double render
+//        if(event.getStage() == RenderLevelStageEvent.Stage.AFTER_PARTICLES){
+//            Minecraft mc = Minecraft.getInstance();
+//            int combinedLightIn = mc.getEntityRenderDispatcher().getPackedLightCoords(mc.player, event.getPartialTick());
+//            PoseStack matrix = event.getPoseStack();
+//            MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+////            matrix.pushPose();
+////            // mirror text
+////            matrix.translate(-mc.player.position().x(), -mc.player.position().y(), -mc.player.position().z());
+////            matrix.scale(1.1F,1F,1F);
+////            // put cords to render text or position
+////            matrix.translate(0, -61, 0);
+////            Quaternion q = mc.getEntityRenderDispatcher().cameraOrientation();
+////
+////            // text scale
+////            matrix.scale(-0.025F, -0.025F, 0.025F);
+////            Matrix4f matrix4f = matrix.last().pose();
+////            matrix.mulPose(q);
+////            Font fontrenderer = Minecraft.getInstance().font;
+////
+////            // found width of your text
+////            float width = (float)(-fontrenderer.width("testText") / 2);
+////            // read about drawInBatch in forgeAPI
+////            fontrenderer.drawInBatch("testText", width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
+////            matrix.popPose();
+//
+//        }
+//
+//    }
+    // TODO use for node render
+//    @SubscribeEvent
+//    public static void renderSelectedBlocks(final RenderLevelStageEvent event) {
+//        if(event.getStage() != RenderLevelStageEvent.Stage.AFTER_PARTICLES)
+//            return;
+//        if(Minecraft.getInstance().player != null && Minecraft.getInstance().player.getMainHandItem().getItem() == ItemInit.THAUMOMETER.get()) {
+//            int combinedLightIn = Minecraft.getInstance().getEntityRenderDispatcher().getPackedLightCoords(Minecraft.getInstance().player, event.getPartialTick());
+//            PoseStack matrix = event.getPoseStack();
+//            MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+//            VertexConsumer builder = buffer.getBuffer(RenderTypes.THICK_LINES);
+//            Vec3 playerPos = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+//            BlockPos blocks = new BlockPos(0,-61, 0);
+//            AABB box = new AABB(blocks);
+//            Matrix4f matrix4f = matrix.last().pose();
+//            matrix.pushPose();
+//            matrix.translate(-playerPos.x(), -playerPos.y(), -playerPos.z());
+//            matrix.scale(1.1F,1F,1F);
+//            LevelRenderer.renderLineBox(matrix, builder, box, 1.0F, 0.0F, 0.0F, 1.0F);
+////            new ItemRenderer().;
+//
+////            IClientItemExtensions.FontContext fontrenderer;
+//            float width = (float)(-Minecraft.getInstance().font.width("test") / 2);
+//            TranslatableContents text = new TranslatableContents("Test");
+////            Minecraft.getInstance().font.drawInBatch("test", width, 1F,0,false, matrix4f, buffer, true, 0, combinedLightIn);
+////            FontRenderer fontrenderer = Minecraft.getInstance().font;
+////            fontrenderer.drawInBatch(text, width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
+////            LevelRenderer.renderLineBox(matrix,builder, 1D,0D,1D,1D,0D,0D, 1.0F, 0.0F, 0.0F, 1.0F);
+//            renderNameTag(matrix, buffer, event.getPartialTick(), 0.0F);
+//            matrix.popPose();
+//            buffer.endBatch(RenderTypes.THICK_LINES);
+//        }
+//    }
+//
+//    private static void renderNameTag(PoseStack ms, MultiBufferSource.BufferSource buffer, float partialTicks, float yOffset) {
+//        Minecraft mc = Minecraft.getInstance();
+//        var playerLookX = mc.player.getLookAngle().x;
+//        var playerLookY = mc.player.getLookAngle().y;
+//        var playerLookZ = mc.player.getLookAngle().z;
+//        Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
+//        int combinedLightIn = mc.getEntityRenderDispatcher().getPackedLightCoords(mc.player, partialTicks);
+//        ms.pushPose();
+// /*       System.out.println(mc.player.getX());*/
+//        ms.translate(0, -61, 0);
+//        Quaternion q = mc.getEntityRenderDispatcher().cameraOrientation();
+//        ms.mulPose(q);
+//        ms.scale(-0.025F, -0.025F, 0.025F);
+//        Matrix4f matrix4f = ms.last().pose();
+//
+//        float opacity = Minecraft.getInstance().options.getBackgroundOpacity(0.5F);
+//        int alpha = (int)(opacity * 255.0F) << 24;
+//        Font fontrenderer = Minecraft.getInstance().font;
+//        float width = (float)(-fontrenderer.width("test") / 2);
+//        //fontrenderer.drawInBatch(text, f2, i, 553648127, false, matrix4f, buffer, false, j, p_225629_5_);
+//
+//        //fontrenderer.drawInBatch(text, width, 0f, 553648127, false, matrix4f, buffer, false, alpha, combinedLightIn);
+//        fontrenderer.drawInBatch("test", width, 0f, -1, false, matrix4f, buffer, true, 0, combinedLightIn);
+//        ms.popPose();
+//    }
 
 //    @SubscribeEvent
 //    public static void onPostDrawOverlay(ScreenEvent.Render event){
