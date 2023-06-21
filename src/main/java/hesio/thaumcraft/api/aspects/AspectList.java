2d25694fca8fc05df8ba4b0af985c81b2abbff02
PrimalCat-Real <romanenko.denis2003@gmail.com>
2023-06-21 14:41:45 +0000

    make serializer

--- src/main/java/hesio/thaumcraft/api/aspects/AspectList.java
+++ src/main/java/hesio/thaumcraft/api/aspects/AspectList.java
@@ -1,19 +1,24 @@
 package hesio.thaumcraft.api.aspects;
 
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
 
-public class AspectList{
+public class AspectList implements JsonSerializer<AspectList>, JsonDeserializer<AspectList> {
     public LinkedHashMap<Aspect,Integer> aspects;
+
 
     public AspectList(){
         this.aspects = new LinkedHashMap<Aspect,Integer>();
     }
 
-    public void add(Aspect aspect, int amount){
-        if (aspect != null || amount > 0) {
+    public AspectList add(Aspect aspect, int amount) {
+        if (aspect != null && amount > 0) {
             int currentAmount = aspects.getOrDefault(aspect, 0);
             aspects.put(aspect, currentAmount + amount);
         }
+        return this;
     }
 
 
@@ -34,4 +39,18 @@ public class AspectList{
         return sb.toString();
     }
 
+    @Override
+    public AspectList deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return null;
+    }
+
+    @Override
+    public JsonElement serialize(AspectList src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject jsonObject = new JsonObject();
+        for (Aspect aspect : src.aspects.keySet()) {
+            int amount = src.aspects.get(aspect);
+            jsonObject.addProperty(aspect.getName(), amount);
+        }
+        return jsonObject;
+    }
 }
