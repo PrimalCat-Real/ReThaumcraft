8c5c1e21c3d389a2c7aec8c8b28010b1ba7b898a
PrimalCat-Real <romanenko.denis2003@gmail.com>
2023-07-11 15:35:22 +0000

    remove test obj blockitem

---
+++ src/main/java/hesio/thaumcraft/common/Items/ItemBase.java
@@ -0,0 +1,10 @@
+package hesio.thaumcraft.common.Items;
+
+import net.minecraft.world.item.Item;
+
+public abstract class ItemBase extends Item {
+    public ItemBase(Properties props) {
+        super(props);
+//        setCreativeTab(CreativeModeTab.TAB_BREWING);
+    }
+}
