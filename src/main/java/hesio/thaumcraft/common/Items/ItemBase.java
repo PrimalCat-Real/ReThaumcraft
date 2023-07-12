2cc36c71e04fa1b72100264241b09a1b0bbb1438
PrimalCat-Real <romanenko.denis2003@gmail.com>
2023-07-12 13:01:01 +0000

    add use with hold for scanner

--- src/main/java/hesio/thaumcraft/common/Items/ItemBase.java
+++ src/main/java/hesio/thaumcraft/common/Items/ItemBase.java
@@ -1,10 +1,21 @@
 package hesio.thaumcraft.common.Items;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public abstract class ItemBase extends Item {
+
     public ItemBase(Properties props) {
         super(props);
 //        setCreativeTab(CreativeModeTab.TAB_BREWING);
     }
+
+
+
 }
--- src/main/java/hesio/thaumcraft/common/Items/tools/Thaumometer.java
+++ src/main/java/hesio/thaumcraft/common/Items/tools/Thaumometer.java
@@ -1,11 +1,88 @@
 package hesio.thaumcraft.common.Items.tools;
 
 import hesio.thaumcraft.common.Items.ItemBase;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
 
 public class Thaumometer extends ItemBase {
     public Thaumometer() {
         super(new Properties().stacksTo(1).rarity(Rarity.UNCOMMON));
     }
+
+
+    private static final int RIGHT_CLICK_HOLD_DURATION = 40;
+
+    private boolean isHoldingRightClick = false;
+
+    public InteractionResult useOn(UseOnContext context) {
+        Player player = context.getPlayer();
+        if (player != null) {
+            player.startUsingItem(context.getHand());
+        }
+
+        return InteractionResult.CONSUME;
+    }
+
+//    @Override
+//    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+//        if (hand == InteractionHand.MAIN_HAND) {
+//            System.out.println("remain tiks "+ player.getUseItemRemainingTicks());
+//            if(player.getUseItemRemainingTicks() == 0){
+//                player.startUsingItem(hand);
+//            }
+//
+//            System.out.println(this.getUseDuration(player.getItemInHand(hand)));
+//            // Reset the hold ticks counter when the player starts using the item
+//        }
+//        return super.use(level, player, hand);
+//    }
+
+
+
+
+    @Override
+    public void onUseTick(Level level, LivingEntity entity, ItemStack stack, int count) {
+        if (count == 1) { // Every second (20 ticks = 1 second)
+            // Do something
+            System.out.println("Test from use tick " + count);
+        }
+        System.out.println("tick " + count);
+//        System.out.println("Test from use tick");
+    }
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+        ItemStack resultStack = super.finishUsingItem(stack, level, entity);
+        if (this.isEdible()) {
+            System.out.println("Item finished using: " + stack.getItem());
+            // Add your desired logging or code here
+        }
+        return resultStack;
+    }
+    @Override
+    public int getUseDuration(ItemStack itemStack) {
+        return this.RIGHT_CLICK_HOLD_DURATION;
+    }
+    @Override
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity entity, int duration) {
+        super.releaseUsing(stack, level, entity, duration);
+        System.out.println("dur: "+duration);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, net.minecraft.world.entity.Entity entity, int slot, boolean isSelected) {
+        if (level.isClientSide && entity instanceof Player player) {
+            if (player.getItemInHand(player.getUsedItemHand()).getItem() == this.asItem()){
+
+            }
+        }
+    }
+
 }
