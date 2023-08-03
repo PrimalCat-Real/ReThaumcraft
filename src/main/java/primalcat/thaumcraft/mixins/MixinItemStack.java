35c5ef9ebe08e8e3d873b6796465392336f1e362
PrimalCat-Real <romanenko.denis2003@gmail.com>
2023-08-03 15:13:17 +0000

    remove nbt aspect system

--- src/main/java/primalcat/thaumcraft/mixins/MixinItemStack.java
+++ src/main/java/primalcat/thaumcraft/mixins/MixinItemStack.java
@@ -27,21 +27,21 @@ public class MixinItemStack {
     @Inject(method = "<init>(Lnet/minecraft/world/level/ItemLike;)V", at = @At("RETURN"))
     private void modifyNBT(ItemLike itemLike, CallbackInfo ci) throws IOException {
         // Check if aspects exist for the specific item
-        if (itemLike != null && ConfigAspects.getReadConfig() != null) {
-            Item actualItem = itemLike.asItem();
-            Map<String, AspectList> aspectItems = ConfigAspects.getReadConfig().get("items");
-            if(aspectItems != null && aspectItems.containsKey(actualItem.toString())){
-                ListTag list = new ListTag();
-                LinkedHashMap<Aspect, Integer> aspects = aspectItems.get(actualItem.toString()).aspects;
-                aspects.forEach((aspect, amount) -> {
-                    CompoundTag aspectTag = new CompoundTag();
-                    aspectTag.putInt(aspect.getName(), amount);
-                    list.add(aspectTag);
-                });
-                CompoundTag nbt = new CompoundTag();
-                nbt.put("aspects", list);
-                ((ItemStack)(Object)this).setTag(nbt);
-            }
-        }
+//        if (itemLike != null && ConfigAspects.getReadConfig() != null) {
+//            Item actualItem = itemLike.asItem();
+//            Map<String, AspectList> aspectItems = ConfigAspects.getReadConfig().get("items");
+//            if(aspectItems != null && aspectItems.containsKey(actualItem.toString())){
+//                ListTag list = new ListTag();
+//                LinkedHashMap<Aspect, Integer> aspects = aspectItems.get(actualItem.toString()).aspects;
+//                aspects.forEach((aspect, amount) -> {
+//                    CompoundTag aspectTag = new CompoundTag();
+//                    aspectTag.putInt(aspect.getName(), amount);
+//                    list.add(aspectTag);
+//                });
+//                CompoundTag nbt = new CompoundTag();
+//                nbt.put("aspects", list);
+//                ((ItemStack)(Object)this).setTag(nbt);
+//            }
+//        }
     }
 }
