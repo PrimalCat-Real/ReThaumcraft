5507d76de115cd46d16ef970f1e133a40d4a96b7
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-09-04 14:18:59 +0000

    start making custom render text for animation

---
+++ src/main/java/primalcat/thaumcraft/client/render/overlays/TextElement.java
@@ -0,0 +1,54 @@
+package primalcat.thaumcraft.client.render.overlays;
+
+public class TextElement {
+    private final String text;
+    private float opacity;
+    private boolean fadingIn;
+    private boolean fadingOut;
+
+    private static int color;
+
+    public static int getColor() {
+        return color;
+    }
+
+    public static void setColor(int color) {
+        TextElement.color = color;
+    }
+
+    public TextElement(String text, int color) {
+        this.text = text;
+        this.opacity = 0.0F;
+        this.fadingIn = true;
+        this.fadingOut = false;
+        this.color = color;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public float getOpacity() {
+        return opacity;
+    }
+
+    public void setOpacity(float opacity) {
+        this.opacity = opacity;
+    }
+
+    public boolean isFadingIn() {
+        return fadingIn;
+    }
+
+    public void setFadingIn(boolean fadingIn) {
+        this.fadingIn = fadingIn;
+    }
+
+    public boolean isFadingOut() {
+        return fadingOut;
+    }
+
+    public void setFadingOut(boolean fadingOut) {
+        this.fadingOut = fadingOut;
+    }
+}
--- src/main/java/primalcat/thaumcraft/client/render/overlays/ThaumcraftOverlay.java
+++ src/main/java/primalcat/thaumcraft/client/render/overlays/ThaumcraftOverlay.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import org.w3c.dom.Text;
 import primalcat.thaumcraft.client.ScanManager;
 
 import java.util.ArrayList;
@@ -16,43 +17,53 @@ import java.util.List;
 
 public class ThaumcraftOverlay implements IGuiOverlay {
 
-    private static List<String> textForRender = new ArrayList<>();
+    private static List<TextElement> textForRender = new ArrayList<>();
 
-    private static int color = 0xFFFFFF;
-    public static void addTextForRender(String text){
-        textForRender.add(text);
+
+    public static void addTextForRender(String text) {
+        textForRender.add(new TextElement(text, 0xFFFFFF));
     }
 
+    public static void addTextForRender(String text, int color) {
+        textForRender.add(new TextElement(text, color));
+    }
     @Override
     public void render(ForgeGui gui, PoseStack poseStack, float partialTicks, int width, int height) {
 
         MultiBufferSource buffers = Minecraft.getInstance().renderBuffers().bufferSource();
 
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F); // Set opacity
+
+//        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F); // Set opacity
         Font font = Minecraft.getInstance().font;
         // Custom font rendering code with yOffset
         poseStack.pushPose();
-
+//        System.out.println(partialTicks);
         poseStack.scale(0.5F, 0.5F, 1.0F);
         int i = 0;
-        for (String textItem: textForRender) {
-            int textWidth = font.width(textItem);
-
+        for (TextElement textItem: textForRender) {
+            int textWidth = font.width(textItem.getText());
+            float opacity = textItem.getOpacity();
+//            if (textItem.isFadingIn()) {
+//                opacity = Math.min(opacity + partialTicks / 20.0F, 1.0F);
+//                textItem.setOpacity(opacity);
+//                if (opacity >= 1.0F) {
+//                    textItem.setFadingIn(false);
+//                }
+//            }
             // Calculate the X-coordinate for centered text
 //        int x = 0 - textWidth;
             int x = ((width - 3) - textWidth / 4) * 2;
-
             int y = ((height - 3) - font.lineHeight - i * font.lineHeight) * 2;
-
-
             // Use GuiComponent.drawCenteredString to draw centered text
-            GuiComponent.drawCenteredString(poseStack, font, textItem, x, y, 0xFFFFFF);
+//            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, opacity);
+//
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderColor(.7f, .7f, .8f, 0.25f);
+            GuiComponent.drawCenteredString(poseStack, font, textItem.getText(), x, y, textItem.getColor());
             i += 1;
         }
-
-
-
         poseStack.popPose();
 
     }
