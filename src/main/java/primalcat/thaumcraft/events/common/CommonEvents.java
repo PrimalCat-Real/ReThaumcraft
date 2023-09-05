fee156ae874ae38868f4a00618440913b7f3ad40
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-09-05 10:57:53 +0000

    rename aspects

--- src/main/java/primalcat/thaumcraft/events/common/CommonEvents.java
+++ src/main/java/primalcat/thaumcraft/events/common/CommonEvents.java
@@ -1,20 +1,15 @@
 package primalcat.thaumcraft.events.common;
 
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import primalcat.thaumcraft.Thaumcraft;
-import primalcat.thaumcraft.aspects.AspectList;
-import primalcat.thaumcraft.client.ScanManager;
 import primalcat.thaumcraft.common.capability.aspects.PlayerAspectsProvider;
 import primalcat.thaumcraft.config.ThaumcraftClientConfig;
 import primalcat.thaumcraft.init.AspectInit;
 import primalcat.thaumcraft.networking.PacketManager;
-import primalcat.thaumcraft.networking.packets.PlayerAspectsActionPacket;
 import primalcat.thaumcraft.networking.packets.PlayerAspectsSyncS2CPacket;
 import primalcat.thaumcraft.networking.packets.PlayerTargetsSyncS2CPacket;
 
@@ -30,11 +25,11 @@ public class CommonEvents {
                     if(playerAspectsData.getAspects().isEmpty()){
                         // firth time join give player primal aspects
                         playerAspectsData.addAspect(AspectInit.AIR.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
-                        playerAspectsData.addAspect(AspectInit.FIRE.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
-                        playerAspectsData.addAspect(AspectInit.EARTH.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
-                        playerAspectsData.addAspect(AspectInit.WATER.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
+                        playerAspectsData.addAspect(AspectInit.IGNIS.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
+                        playerAspectsData.addAspect(AspectInit.TERRA.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
+                        playerAspectsData.addAspect(AspectInit.AQUA.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
                         playerAspectsData.addAspect(AspectInit.ORDER.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
-                        playerAspectsData.addAspect(AspectInit.ENTROPY.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
+                        playerAspectsData.addAspect(AspectInit.PERDITIO.getName(), ThaumcraftClientConfig.PLAYER_PRIMAL_ASPECTS_COUNT.get());
                     }
                     PacketManager.sendToPlayer(new PlayerTargetsSyncS2CPacket(playerAspectsData.getTargetsList()), player);
                     PacketManager.sendToPlayer(new PlayerAspectsSyncS2CPacket(playerAspectsData.getAspects()), player);
