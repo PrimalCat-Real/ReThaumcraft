64e3a6c285362d654ae2fb5a0bb6db240a704c45
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-09-05 16:47:42 +0000

    test custom element

---
+++ src/main/java/primalcat/thaumcraft/client/render/overlays/CustomElement.java
@@ -0,0 +1,12 @@
+package primalcat.thaumcraft.client.render.overlays;
+
+import net.minecraft.resources.ResourceLocation;
+import primalcat.thaumcraft.Thaumcraft;
+
+public class CustomElement {
+
+    public int positionX = 0;
+    public int positionY = 0;
+
+    public ResourceLocation texture = new ResourceLocation(Thaumcraft.MOD_ID, "textures/items/thaumonomicon.png");
+}
