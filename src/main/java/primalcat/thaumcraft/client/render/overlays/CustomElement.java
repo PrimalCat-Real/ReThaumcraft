019782f25e73bff3e5e099d561b31c410dc53ee6
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-09-07 09:20:33 +0000

    working aspect animation for scanned target (@TODO check null for aspects.keySet())

--- src/main/java/primalcat/thaumcraft/client/render/overlays/CustomElement.java
+++ src/main/java/primalcat/thaumcraft/client/render/overlays/CustomElement.java
@@ -1,12 +1,88 @@
 package primalcat.thaumcraft.client.render.overlays;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import primalcat.thaumcraft.Thaumcraft;
 
 public class CustomElement {
 
-    public int positionX = 0;
-    public int positionY = 0;
+    private int currentX = 0;
+    private int currentY = 0;
+    private int color;
+    private PoseStack poseStack;
+    private int width;
+    private int height;
 
-    public ResourceLocation texture = new ResourceLocation(Thaumcraft.MOD_ID, "textures/items/thaumonomicon.png");
+    private int spreadX = (int) (Math.random() * 100);
+    private int spreadY = (int) (Math.random() * 100 - 100);
+
+    public int index = 0;
+    public boolean isRender = true;
+    public ResourceLocation texture;
+
+//    public ResourceLocation texture = new ResourceLocation(Thaumcraft.MOD_ID, "textures/items/thaumonomicon.png");
+
+    public CustomElement(int color, PoseStack poseStack, int width, int height, ResourceLocation texture) {
+        this.color = color;
+        this.poseStack = poseStack;
+        this.width = width;
+        this.height = height;
+        this.texture = texture;
+    }
+
+    public void render(){
+
+//        while (isRender){
+
+        int startPointX = width / 2 - 8  - spreadX;
+        int startPointY = height / 2 - 8 - spreadY;
+
+        int controlPointX = (int) (width / 1.4) - spreadX;
+        int controlPointY = height / 3 - spreadY;
+
+        int endPointX = width - 16 - 8;
+        int endPointY = 8;
+
+        int maxIndex = 100; // You can adjust this value based on your desired animation speed
+
+// Calculate the position along the quadratic Bézier curve
+        float t = (float) index / maxIndex;
+        float u = 1 - t;
+        int currentX = (int) (u * u * startPointX + 2 * u * t * controlPointX + t * t * endPointX);
+        int currentY = (int) (u * u * startPointY + 2 * u * t * controlPointY + t * t * endPointY);
+
+// Clamp currentX and currentY to stay within bounds
+        currentX = Math.max(0, Math.min(currentX, 1920));
+        currentY = Math.max(0, Math.min(currentY, 1080));
+
+// Render the texture at the current position (x, y)
+        int scale;
+        float opacity;
+        if (index < 50) {
+            opacity = 0.5f + ((float) index / 50.0f) * 0.5f; // Change opacity from 0.5 to 1
+            scale = (int) (16 + (float) index * 0.16f);
+        } else {
+            opacity = 1.0f - ((float) (index - 50) / 50.0f) * 0.5f; // Change opacity from 1 to 0
+            scale = (int) (24 - (float) (index - 50) * 0.32f); // Change scale from 24 to 8
+        }
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        float red = (( color >> 16) & 0xFF) / 255.0f;
+        float green = (( color >> 8) & 0xFF) / 255.0f;
+        float blue = ( color & 0xFF) / 255.0f;
+        RenderSystem.setShaderColor(red, green, blue, opacity);
+        RenderSystem.setShaderTexture(0,texture);
+        GuiComponent.blit(poseStack, currentX, currentY, 0, 0, scale, scale, scale, scale);
+        index +=1;
+//            if (opacity == 0){
+//                this.isRender = false;
+//            }
+//        }
+
+
+
+    }
 }
--- src/main/java/primalcat/thaumcraft/client/render/overlays/ThaumcraftOverlay.java
+++ src/main/java/primalcat/thaumcraft/client/render/overlays/ThaumcraftOverlay.java
@@ -19,10 +19,8 @@ import primalcat.thaumcraft.aspects.AspectListIterator;
 import primalcat.thaumcraft.client.ScanManager;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class ThaumcraftOverlay implements IGuiOverlay {
 
@@ -30,11 +28,17 @@ public class ThaumcraftOverlay implements IGuiOverlay {
 
     private static List<TextElement> textForRender = new ArrayList<>();
 
-    private static CustomElement test = new CustomElement();
 
     private static Iterator<Map.Entry<Aspect, Integer>> iterator;
 
+    private static Aspect currentAspect = null;
+    private static int animationIndex = 0;
+
+    private static float fixTick = 0;
+
     private static AspectList aspectsForRender = new AspectList();
+
+    private static List<CustomElement> activeRenderAspects = new ArrayList<>();
 
     public static AspectList getAspectsForRender() {
         return aspectsForRender;
@@ -53,85 +57,41 @@ public class ThaumcraftOverlay implements IGuiOverlay {
     }
     @Override
     public void render(ForgeGui gui, PoseStack poseStack, float partialTicks, int width, int height) {
-        MultiBufferSource buffers = Minecraft.getInstance().renderBuffers().bufferSource();
-        Font font = Minecraft.getInstance().font;
         poseStack.pushPose();
-//        poseStack.scale(0.5F, 0.5F, 1.0F);
-//        if (aspectsForRender.aspects.keySet().size() > index){
-//            RenderSystem.enableBlend();
-//            RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-//            RenderSystem.setShaderTexture(0, aspectsForRender.aspects.keySet());
-//            GuiComponent.blit(poseStack, positionX, positionY, 0, 0, 16, 16, 16, 16);
-//        }
-        if(aspectsForRender != null && !aspectsForRender.isEmpty()){
-            iterator = new AspectListIterator(aspectsForRender);
-//            while (iterator.hasNext()) {
-//                Map.Entry<Aspect, Integer> entry = iterator.next();
-//                Aspect aspect = entry.getKey();
-//                int amount = entry.getValue();
-//
-//                // Process the Aspect and amount as needed
-//                System.out.println("Aspect: " + aspect.getName() + ", Amount: " + amount);
-//            }
+        if (aspectsForRender != null && !aspectsForRender.isEmpty()) {
             RenderSystem.disableBlend();
             RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(Thaumcraft.MOD_ID, "textures/items/thaumonomicon.png"));
             GuiComponent.blit(poseStack, width - 16 - 8,  8, 0, 0, 16, 16, 16, 16);
-
-            // test animation
-            if(iterator.hasNext()){
-                Aspect tempAspect = iterator.next().getKey();
-                RenderSystem.setShaderTexture(0, tempAspect.getAspectImage());
-
-
-//            double randomValue = Math.random(); // Generates a random value between 0 (inclusive) and 1 (exclusive)
-//            double scaledValue = randomValue * 40 - 20;
-
-                int startPointX = width / 2 - 8;
-                int startPointY = height / 2 - 8;
-
-                int controlPointX = (int) (width / 1.4);
-                int controlPointY = height / 3;
-
-                int endPointX = width - 16 - 8;
-                int endPointY = 8;
-
-                int maxIndex = 100; // You can adjust this value based on your desired animation speed
-
-// Calculate the position along the quadratic Bézier curve
-                float t = (float) index / maxIndex;
-                float u = 1 - t;
-                int currentX = (int) (u * u * startPointX + 2 * u * t * controlPointX + t * t * endPointX);
-                int currentY = (int) (u * u * startPointY + 2 * u * t * controlPointY + t * t * endPointY);
-
-// Clamp currentX and currentY to stay within bounds
-                currentX = Math.max(0, Math.min(currentX, 1920));
-                currentY = Math.max(0, Math.min(currentY, 1080));
-
-// Render the texture at the current position (x, y)
-                int scale;
-                float opacity;
-                if (index < 50) {
-                    opacity = 0.5f + ((float) index / 50.0f) * 0.5f; // Change opacity from 0.5 to 1
-                    scale = (int) (16 + (float) index * 0.16f);
-                } else {
-                    opacity = 1.0f - ((float) (index - 50) / 50.0f) * 0.5f; // Change opacity from 1 to 0
-                    scale = (int) (24 - (float) (index - 50) * 0.32f); // Change scale from 24 to 8
+            if(fixTick > 10){
+                fixTick = 0;
+                System.out.println("Tick clear");
+                for (Aspect renderAspect: aspectsForRender.aspects.keySet()) {
+                    if( aspectsForRender.aspects.get(renderAspect) != null && aspectsForRender.aspects.get(renderAspect) == 0){
+                        aspectsForRender.aspects.remove(renderAspect);
+                    }
+                    if(aspectsForRender.aspects.get(renderAspect) != null){
+                        aspectsForRender.aspects.put(renderAspect, aspectsForRender.aspects.get(renderAspect) - 1);
+                        activeRenderAspects.add(new CustomElement(renderAspect.getColor(),poseStack, width, height, renderAspect.getAspectImage()));
+                    }
                 }
-                RenderSystem.enableBlend();
-                RenderSystem.defaultBlendFunc();
-                int color = tempAspect.getColor();
-                float red = (( color >> 16) & 0xFF) / 255.0f;
-                float green = (( color >> 8) & 0xFF) / 255.0f;
-                float blue = ( color & 0xFF) / 255.0f;
-                RenderSystem.setShaderColor(red, green, blue, opacity);
-                GuiComponent.blit(poseStack, currentX, currentY, 0, 0, scale, scale, scale, scale);
-                index += 1;
+            }else {
+                fixTick += partialTicks;
             }
-
+        }
+        if(activeRenderAspects != null && !activeRenderAspects.isEmpty()){
+            RenderSystem.setShaderTexture(0, new ResourceLocation(Thaumcraft.MOD_ID, "textures/items/thaumonomicon.png"));
+            for (CustomElement renderElement: activeRenderAspects) {
+                renderElement.render();
+            }
         }
 
-        // Initialize the iterator and current index
+        poseStack.popPose();
+    }
+
+}
+
+
+// Initialize the iterator and current index
 //        Iterator<AspectList> textIterator = aspectsForRender.iterator();
 //        int currentIndex = 0;
 //        double v = (height / 2) / Math.pow(width / 2, 2);
@@ -176,7 +136,3 @@ public class ThaumcraftOverlay implements IGuiOverlay {
 //            GuiComponent.drawCenteredString(poseStack, font, textItem.getText(), x, y, new Color(234, 186, 43, opacityInt).getRGB());
 //            i += 1;
 //        }
-        poseStack.popPose();
-    }
-
-}
