31ba60758e886f3d92f0e29fdb133b2acea07b42
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-09-07 16:15:02 +0000

    packet to sync c2s scanned targets

---
+++ src/main/java/primalcat/thaumcraft/networking/packets/PlayerTargetsSyncC2SPacket.java
@@ -0,0 +1,67 @@
+package primalcat.thaumcraft.networking.packets;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraftforge.network.NetworkEvent;
+import primalcat.thaumcraft.aspects.AspectList;
+import primalcat.thaumcraft.client.ScanManager;
+import primalcat.thaumcraft.common.capability.aspects.PlayerAspectsProvider;
+import primalcat.thaumcraft.networking.PacketManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public class PlayerTargetsSyncC2SPacket {
+
+    private List<String> targets = new ArrayList<>();
+    private UUID playerUUID;
+    public PlayerTargetsSyncC2SPacket(List<String> targets,  UUID playerUUID) {
+        this.targets = targets;
+        this.playerUUID = playerUUID;
+    }
+
+    public PlayerTargetsSyncC2SPacket(FriendlyByteBuf buf) {
+        int targetCount = buf.readInt();
+        for (int i = 0; i < targetCount; i++) {
+            targets.add(buf.readUtf());
+        }
+        this.playerUUID = buf.readUUID();
+
+    }
+
+    public void toBytes(FriendlyByteBuf buf) {
+        int targetCount = targets.size();
+        buf.writeInt(targetCount); // Write the number of strings in the list
+
+        for (String target : targets) {
+            buf.writeUtf(target); // Write each string in the list
+        }
+        buf.writeUUID(playerUUID);
+    }
+
+    public boolean handle(Supplier<NetworkEvent.Context> supplier) {
+        NetworkEvent.Context context = supplier.get();
+        context.enqueueWork(() -> {
+            // HERE WE ARE ON THE SERVER!
+            MinecraftServer minecraftServer = context.getSender().getServer();
+            ServerPlayer serverPlayer = minecraftServer.getPlayerList().getPlayer(playerUUID);
+            if(serverPlayer != null){
+                serverPlayer.getCapability(PlayerAspectsProvider.PLAYER_ASPECTS).ifPresent(aspectsProvider -> {
+                    aspectsProvider.clearTargets();
+                    aspectsProvider.mergeTargets(targets);
+                });
+                PacketManager.sendToPlayer(new PlayerTargetsSyncS2CPacket(targets), serverPlayer);
+            }
+
+
+        });
+        return true;
+    }
+}
