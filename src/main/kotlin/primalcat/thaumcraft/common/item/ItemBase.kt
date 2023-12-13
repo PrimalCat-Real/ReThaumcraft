a609d7a1e04f266d040b3ea7ff823038d6f4d175
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-12-13 16:38:33 +0000

    create empty basic item class

---
+++ src/main/kotlin/primalcat/thaumcraft/common/item/ItemBase.kt
@@ -0,0 +1,9 @@
+package primalcat.thaumcraft.common.item
+import net.minecraft.world.item.Item
+import primalcat.thaumcraft.common.init.ThaumcraftCreativeTab
+
+open class ItemBase(props: Properties) : Item(props){
+    init {
+
+    }
+}
