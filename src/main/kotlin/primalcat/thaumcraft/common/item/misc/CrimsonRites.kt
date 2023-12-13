69da153246857984e98f64142415001635ce342c
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-12-13 16:39:05 +0000

    create empty crimson rites essence item class

---
+++ src/main/kotlin/primalcat/thaumcraft/common/item/misc/CrimsonRites.kt
@@ -0,0 +1,9 @@
+package primalcat.thaumcraft.common.item.misc
+
+import net.minecraft.world.item.Rarity
+import primalcat.thaumcraft.common.item.ItemBase
+
+class CrimsonRites : ItemBase(
+    props = Properties()
+) {
+}
