0467e9e89fd0c60f003dfc008dd1b2d29ffc5f0b
PrimalCatReal <romanenko.denis2003@gmail.com>
2023-12-13 16:39:17 +0000

    create empty bottle taint essence item class

---
+++ src/main/kotlin/primalcat/thaumcraft/common/item/misc/BottleTaint.kt
@@ -0,0 +1,11 @@
+package primalcat.thaumcraft.common.item.misc
+
+import net.minecraft.world.item.Rarity
+import primalcat.thaumcraft.common.item.ItemBase
+
+class BottleTaint : ItemBase(
+    props = Properties()
+        .stacksTo(1)
+        .rarity(Rarity.UNCOMMON)
+) {
+}
